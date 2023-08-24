9a2e3c5115529246a76c36b2a27fb446890a4605
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-24 13:20:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+    const listItemWithId = document.getElementById("level");
+      const ulElement = listItemWithId.closest("ul");
+      const listItems = ulElement.getElementsByTagName("li");
+
+      let listItemNumber = -1;
+      for (let i = 0; i < listItems.length; i++) {
+          if (listItems[i].id === 'level') {
+            
+          listItemNumber = i + 1; // Adding 1 to make it a human-readable number
+          break;
+        }
+      }
+
+alert(`The level of the element is: ${listItemNumber}`);
